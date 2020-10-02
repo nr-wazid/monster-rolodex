92a4a58504c59a04bbb0b915ddcc7996c60ad52a
Naimur Rahman Wazid <62604953+nr-wazid@users.noreply.github.com>
2020-10-02 10:28:34 +0600

    Updates

---
+++ precache-manifest.b64c9b606c8bc091ab5e392ca9a5ba6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63632c609f3da1a4fb0e516212640f02",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "78e36a0397990d8d3bc1",
+    "url": "/monster-rolodex/static/css/main.6f05e066.chunk.css"
+  },
+  {
+    "revision": "06f353230f5cd5a5b031",
+    "url": "/monster-rolodex/static/js/2.06df974f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.06df974f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78e36a0397990d8d3bc1",
+    "url": "/monster-rolodex/static/js/main.1d310a57.chunk.js"
+  },
+  {
+    "revision": "e84804a8bcd623453054",
+    "url": "/monster-rolodex/static/js/runtime-main.c601fb62.js"
+  }
+]);
